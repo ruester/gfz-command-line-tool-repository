472f5c30a672dc72f1ac2bede3010ec0381ff0ca
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2019-03-21 14:24:52 +0100

    Adding licence

--- src/main/java/org/n52/gfz/riesgos/cmdexecution/IExecutionRunResult.java
+++ src/main/java/org/n52/gfz/riesgos/cmdexecution/IExecutionRunResult.java
@@ -1,5 +1,21 @@
 package org.n52.gfz.riesgos.cmdexecution;
 
+/*
+ * Copyright (C) 2019 GFZ German Research Centre for Geosciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
 /**
  * Interface to represent a result of a process.
  */
--- src/main/java/org/n52/gfz/riesgos/functioninterfaces/IStdoutHandler.java
+++ src/main/java/org/n52/gfz/riesgos/functioninterfaces/IStdoutHandler.java
@@ -1,6 +1,20 @@
 package org.n52.gfz.riesgos.functioninterfaces;
 
-import org.n52.wps.io.data.IData;
+/*
+ * Copyright (C) 2019 GFZ German Research Centre for Geosciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 
 /**
  * Handler for stdout text
@@ -10,7 +24,7 @@ public interface IStdoutHandler {
 
     /**
      * Handles the text on stdout
-     * @param stdout
+     * @param stdout text from stdout
      */
     void handleStdout(final String stdout);
 }
