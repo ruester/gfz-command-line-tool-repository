4dcee94e834c33e81790b3d055584351da9bb0ea
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2019-04-11 11:02:35 +0200

    equals for one byte array to idata converter (geotiff)

--- src/main/java/org/n52/gfz/riesgos/bytetoidataconverter/ConvertBytesToGeotiffBinding.java
+++ src/main/java/org/n52/gfz/riesgos/bytetoidataconverter/ConvertBytesToGeotiffBinding.java
@@ -28,6 +28,7 @@ import org.n52.wps.io.data.binding.complex.GeotiffBinding;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Function to create a GeotiffBinding from a byte array
@@ -50,5 +51,21 @@ public class ConvertBytesToGeotiffBinding implements IConvertByteArrayToIData {
         } catch(final IOException ioException) {
             throw new ConvertToIDataException(ioException);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass().getName());
     }
 }
---
+++ src/test/java/org/n52/gfz/riesgos/bytetoidataconverter/TestConvertBytesToGeotiffBinding.java
@@ -0,0 +1,41 @@
+package org.n52.gfz.riesgos.bytetoidataconverter;
+
+/*
+ * Copyright (C) 2019 GFZ German Research Centre for Geosciences
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the Licence is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the Licence for the specific language governing permissions and
+ *  limitations under the Licence.
+ *
+ *
+ */
+
+import org.junit.Test;
+import org.n52.gfz.riesgos.functioninterfaces.IConvertByteArrayToIData;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Test class for ConvertBytesToGeotiffBinding
+ */
+public class TestConvertBytesToGeotiffBinding {
+
+    /**
+     * Tests if two instances are equal
+     */
+    @Test
+    public void testEquals() {
+        final IConvertByteArrayToIData converter1 = new ConvertBytesToGeotiffBinding();
+        final IConvertByteArrayToIData converter2 = new ConvertBytesToGeotiffBinding();
+
+        assertEquals("Both are the same", converter1, converter2);
+    }
+}
